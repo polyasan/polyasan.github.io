5c3313decc48eb7fdf9d476f8ee9ddc653eaf361
polyasan <polyasan@gmail.com>
2021-06-14 18:33:27 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import cvData from "./data";
+import Block from "./block";
+import Footer from "./Footer"
+
+function App() {
+  console.log(cvData);
+  return (
+    <div className="App">
+      <img src={process.env.PUBLIC_URL + "profile.png"} width={"200px"} alt="profile" style={{
+        display: "block",
+        position: "fixed",
+        right: "50px",
+        top: "80px"
+      }}/>
+      <div className="w3-container w3-indigo">
+        <h2>Curriculum vitea</h2>
+      </div>
+      {cvData.map((item) => {
+        return (
+          <div>
+            <div className="w3-row">
+              <div className="w3-third w3-container">
+                <h3 className="r">{item.title}</h3>
+              </div>
+            </div>
+            {item.blocks.map((blockItem) => {
+              return <Block block={blockItem} />;
+            })}
+          </div>
+        );
+      })}
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,133 @@
+module.exports = [
+  {
+    title: "Personal information",
+    blocks: [
+      [
+        { label: "First name/surname", value: "Sandor Polyak" },
+        { label: "e-mail", value: "polyasan@gmail.com" },
+        { label: "tel", value: "+36 20 318 8166" },
+        { label: "Date of birth", value: "16.05.1991" },
+      ],
+    ],
+  },
+  {
+    title: "Work experience",
+    blocks: [
+      [
+        { label: "Dates", value: "January 2020 - present" },
+        { label: "Position", value: "Full-stack Web Developer" },
+        { label: "Employer", value: "Self-employed" },
+        {
+          label: "Responsibilities",
+          value: `Front-end development on React platform
+            Back-end development on NodeJS platform
+            Server and database management`,
+        },
+      ],
+      [
+        { label: "Dates", value: "February 2019 - December 2019" },
+        { label: "Position", value: "Senior software engineer" },
+        { label: "Employer", value: "Thyssenkrupp Presta Hungary" },
+        {
+          label: "Responsibilities",
+          value: `Safety critical embedded software and module test development
+          for automotive solutions`,
+        },
+      ],
+      [
+        { label: "Dates", value: "March 2015 - May 2017" },
+        { label: "Position", value: "Embedded hardware engineer" },
+        { label: "Employer", value: "Semilab Zrt." },
+        {
+          label: "Responsibilities",
+          value: `PCB and circuit design for semiconductor industry,
+            Embedded software development`,
+        },
+      ],
+      [
+        { label: "Dates", value: "May 2013 - March 2015" },
+        {
+          label: "Position",
+          value: "Embedded C developer, Front-end developer",
+        },
+        { label: "Employer", value: "Polyprocess Ltd." },
+        {
+          label: "Responsibilities",
+          value: `Software development on ARM platform, front-end
+            development on React platform`,
+        },
+      ],
+      [
+        { label: "Dates", value: "May 2011 - December 2012" },
+        { label: "Position", value: "Embedded C/C++ developer" },
+        { label: "Employer", value: "Székely Invent Ltd." },
+        {
+          label: "Responsibilities",
+          value: "Software development on Atmel platform in C/C++",
+        },
+      ],
+    ],
+  },
+  {
+    title: "Education and training",
+    blocks: [
+      [
+        { label: "Dates", value: "2009-2016" },
+        { label: "Qualification", value: "awarded Bachelors’s degree" },
+        {
+          label: "Principle studies",
+          value: "Electrical engineer, embedded systems",
+        },
+        {
+          label: "Institution",
+          value: "Budapest University of Technology and Economics",
+        },
+      ],
+    ],
+  },
+  {
+    title: "Skills and competences",
+    blocks: [
+      [
+        {
+          label: "Language",
+          value: `English (good command, good working knowledge)
+            Hungarian (native)`,
+        },
+      ],
+      [
+        {
+          label: "Social skills",
+          value: [
+            "I can adapt fast to others",
+            "I am goal oriented",
+            "I have good problem-solving skills",
+            "I have attention to details",
+            "I am open minded",
+            "I am always trying to improve myself",
+          ],
+        },
+      ],
+      [
+        {
+          label: "Computer skills",
+          value: `React JS, Node JS, Mongo DB, MySQL, HTML, CSS, Javascript
+            Other: Visual Code Studio, Docker, Linux, OSX, AtomIO, Bootstrap,
+            NoSQL`,
+        },
+      ],
+      [
+        {
+          label: "Professional skills",
+          value: [
+            "Experience in responsive web development",
+            "Experience in JavaScript based back-end development",
+            "Experience in embedded software development",
+            "Experience in IoT system design",
+            "Experience in PCB design",
+          ],
+        },
+      ],
+    ],
+  },
+];
